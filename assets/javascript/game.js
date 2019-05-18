3260137ed5588a328d93836363e92c3e0a05b3ac
Hibi <correiaph13@gmail.com>
2019-05-17 22:19:55 -0500

    document ready function

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+
+  //declare the array of possible number assignments to append to each crystal.
+
+  $.each(.crystal)function(){
+        var crystalNumber = 1 + Math.floor(Math.random() * 12);
+        console.log(crystalNumber);
+  }
