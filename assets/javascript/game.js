117c091760c64e395a589484cf36b6ab415eda6d
Hibi <correiaph13@gmail.com>
2019-05-17 22:45:39 -0500

    html fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,25 @@
 
+
+ var numberOptions = [10, 5, 3, 7];
+// Next we create a for loop to create crystals for every numberOption.
+//   for (var i = 0; i < numberOptions.length; i++) {
+//     each.crystals.attr("data-crystalvalue", numberOptions[i]);
+//     console.log(.crystals);
+//   }
+
+// pick a random number
+
+//assign each crystal a random number from 1-12
+
+//on click the value of crystal is added to a running total
+
+//if the running total < random number, game continues. if it is > than random number display you lose
+  
+  
+  
   //declare the array of possible number assignments to append to each crystal.
 
-  $.each(.crystal)function(){
-        var crystalNumber = 1 + Math.floor(Math.random() * 12);
-        console.log(crystalNumber);
-  }
+//   $.each(."crystal")function(){
+//         var crystalNumber = 1 + Math.floor(Math.random() * 12);
+//         console.log(crystalNumber);
+//   }
